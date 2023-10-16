6f4619af4646c5b6b50a4f4b28cf290c90d9945f
lokreiiel <mtemkz@gmail.com>
2023-10-16 20:14:37 +0300

    new

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -0,0 +1,23 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import ru.kata.spring.boot_security.demo.models.User;
+
+import java.util.List;
+
+public interface UserService extends UserDetailsService {
+    User findByUsername(String username);
+
+    boolean add(User user);
+
+    List<User> allUsers();
+
+    User update(User user);
+
+    void deleteUser(int id);
+
+    User showUserById(int id);
+
+    UserDetails loadUserByUsername(String username);
+}
